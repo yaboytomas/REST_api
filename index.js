dc2ee78d854a732352c3c9f3f5ab9fe096b266b9
Tomas <tomasszabo94@gmail.com>
2025-06-09 21:15:14 -0400

    too simple

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is alive on http://localhost:${PORT}`);
 }); 
+
+app.get('/tshirt', (req, res) => {
+  res.status(200).send({
+    name: 'Cool T-Shirt',
+    size: 'M',
+    color: 'Blue'
+  });
+}); 
+
+app.post('/tshirt/:id', (req, res) => {
+    const id = req.params.id;
+    res.status(201).send({
+        message: `T-Shirt with ID ${id} created successfully!`
+    });
+});
